1e34f88568b27e1966445a14d50a005f9401231e
shuhejun <shuhejun@halove.com>
2016-10-05 14:01:20 +0800

    add MyFirstProject to my github

---
+++ app/src/main/java/jiuai/com/myfirstproject/SecondActivity.java
@@ -0,0 +1,11 @@
+package jiuai.com.myfirstproject;
+
+/**
+ * author: Administrator
+ * created on: 2016/9/20 14:53
+ * description:
+ */
+
+public class SecondActivity {
+
+}
---
+++ app/src/main/java/jiuai/com/myfirstproject/TagActivity.java
@@ -0,0 +1,26 @@
+package jiuai.com.myfirstproject;
+
+/**
+ * Created by Administrator on 2016/9/20.
+ */
+public class TagActivity {
+
+    /**
+     *
+     */
+    private void get()
+    {
+
+    }
+
+
+    /**
+     *
+     */
+    public void set()
+    {
+
+    }
+
+
+}
